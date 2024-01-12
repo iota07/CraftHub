caea1f1b19920be16cd58a36b0fb998a26b59488
Marine Van Hees <vanheesmarine@gmail.com>
2024-01-12 18:27:46 +0100

    first test routes

---
+++ front/src/Pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Components/Navbar/Navbar';
+
+const Profile = () => {
+    return (
+        <div className='home w-full h-screen bg-yellow'>
+            <Navbar />
+        </div>
+    );
+};
+
+export default Profile;
