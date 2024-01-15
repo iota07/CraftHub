009f9c9e9bfa4dd59c40f1971d44bc3d9213ca4f
Marine Van Hees <vanheesmarine@gmail.com>
2024-01-15 10:32:17 +0100

    change signin form with login from for landing page

--- front/src/Pages/Register.jsx
+++ front/src/Pages/Register.jsx
@@ -5,7 +5,7 @@ import LoginForm from '../Components/LoginForm/LoginForm';
 const Register = () => {
     return (
         <main className='w-full h-screen bg-yellow'>
-            <SignInForm />
+            <LoginForm />
         </main>
     );
 };
