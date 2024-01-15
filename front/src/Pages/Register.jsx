2178ec6abba2d50949ad5431f6a69a62ee9160cd
iota07 <hanesananhouvannasak@googlemail.com>
2024-01-15 14:17:39 +0100

    add modal policy

--- front/src/Pages/Register.jsx
+++ front/src/Pages/Register.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import SignInForm from '../Components/SignInForm/SignInForm';
-import LoginForm from '../Components/LoginForm/LoginForm';
+import React from "react";
+import SignInForm from "../Components/SignInForm/SignInForm";
+import LoginForm from "../Components/LoginForm/LoginForm";
+import GDPRCookieConsent from "../Components/GDPR/GDPRCookieConsent";
 
 const Register = () => {
-    return (
-        <main className='w-full h-screen bg-yellow'>
-            <SignInForm />
-        </main>
-    );
+  return (
+    <main className="w-full h-screen bg-yellow">
+      <GDPRCookieConsent />
+      <SignInForm />
+    </main>
+  );
 };
 
 export default Register;
