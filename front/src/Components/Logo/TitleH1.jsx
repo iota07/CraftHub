ad39f4fefca5b03076844e1e83392fbf5fc67afb
iota07 <hanesananhouvannasak@googlemail.com>
2024-01-15 11:49:45 +0100

    add title in login

--- front/src/Components/Logo/TitleH1.jsx
+++ front/src/Components/Logo/TitleH1.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const TitleH1 = (props) => {
   return (
-    <section className="pt-24 ps-12 pb-8">
-      <h2 className="text-6xl sm:text-2xl md:text-3xl font-dancing lg:text-4xl tracking-tight font-bold text-blue tracking-widest">
+    <section>
+      <h1 className="text-6xl sm:text-2xl md:text-3xl font-dancing lg:text-4xl font-bold text-blue">
         {props.title}
-      </h2>
+      </h1>
     </section>
   );
 };
